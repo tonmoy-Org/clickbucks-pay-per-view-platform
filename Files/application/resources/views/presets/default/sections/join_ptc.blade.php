29e9d3f2fda9204d9fd8989eddf011e259dd1265
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-08-06 21:19:53 +0600

    Update brand name text to PPVBucks across frontend and env

--- Files/application/resources/views/presets/default/sections/join_ptc.blade.php
+++ Files/application/resources/views/presets/default/sections/join_ptc.blade.php
@@ -7,7 +7,7 @@
         <div class="box">
             <div class="d-lg-flex d-md-flex d-block justify-content-between">
                 <h4>{{__(@$joinPtc->data_values->title)}}</h4>
-                <a href="{{route('user.ptc.index')}}" class="btn btn--base mt-3 mt-md-0 mt-lg-0">@lang('Join ClickBucks') <i class="las la-arrow-alt-circle-right"></i></a>
+                <a href="{{route('user.ptc.index')}}" class="btn btn--base mt-3 mt-md-0 mt-lg-0">@lang('Join PPVBucks') <i class="las la-arrow-alt-circle-right"></i></a>
             </div>
         </div>
     </div>
