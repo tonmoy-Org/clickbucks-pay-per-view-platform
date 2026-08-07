5a4e69f7f9414d5f95cdce09169624f69f935fb7
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-08-07 22:46:12 +0600

    Add ads tag.min.js script inside head

--- Files/application/resources/views/presets/default/layouts/frontend.blade.php
+++ Files/application/resources/views/presets/default/layouts/frontend.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ config('app.locale') }}" itemscope itemtype="http://schema.org/WebPage">
 <head>
     <meta name="monetag" content="3ca177cdbd3c8d3af12b3b455777a787">
+    <script src="https://quge5.com/88/tag.min.js" data-zone="267894" async data-cfasync="false"></script>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
