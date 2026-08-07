a806936892bd7903c5021fe17e19612ea4742f8d
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-08-07 20:25:09 +0600

    Remove social media icons list from footer

--- Files/application/resources/views/presets/default/components/footer.blade.php
+++ Files/application/resources/views/presets/default/components/footer.blade.php
@@ -26,11 +26,6 @@ $pages = App\Models\Page::where('tempname',$activeTemplate)->get();
                         {{__(@$contact->data_values->short_details) }}
                         @endif
                     </p>
-                    <ul class="social-list mt-3">
-                        @foreach($socialIcons as $item)
-                        <li class="social-list__item"><a href="{{$item->data_values->url}}" class="social-list__link">@php echo $item->data_values->social_icon; @endphp</a> </li>
-                        @endforeach
-                    </ul>
                     <div class="mt-3">
                         <a href="{{url('/contact')}}" class="btn btn--base btn--sm">@lang('Contact Now')</a>
                     </div>
