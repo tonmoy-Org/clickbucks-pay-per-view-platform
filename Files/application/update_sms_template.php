46e2f454d2502d4bfcf514126de586f8fdf42ad7
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-08-11 23:40:45 +0600

    chore: update sms otp template again to 'Dear'

--- Files/application/update_sms_template.php
+++ Files/application/update_sms_template.php
@@ -9,12 +9,12 @@ use Illuminate\Support\Facades\DB;
 
 // Update global SMS template
 DB::table('general_settings')->update([
-    'sms_body' => "Hi @{{username}} ,\n{{message}}"
+    'sms_body' => "Dear,\n@{{username}}\n\n{{message}}"
 ]);
 
 // Update SVER_CODE
 DB::table('notification_templates')->where('act', 'SVER_CODE')->update([
-    'sms_body' => "PPVBucks Verification\nYour code is {{code}}. Do not share it."
+    'sms_body' => "PPV Bucks Verification \nCode: {{code}}\n\nDo not share it."
 ]);
 
 echo "Done PPVBucks\n";
